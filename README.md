8c2aab82d5b31c74be62c728ef6a700f03cab952
Roni Laukkarinen <roni@laukkarinen.info>
2013-06-06 23:55:14 +0300

    name change and rewrite

---
+++ README.md
@@ -0,0 +1,9 @@
+Composer.io Chrome Extension
+==============
+
+Rest in peace Ping.fm, Welcome Composer.io!
+--------------
+
+Originally a ping.fm extension for Google Chrome, but in spring 2013 developers who missed ping.fm invented composer.io.
+
+This extension is a simple sharing app for Google Chrome, which shares the title and the link to all your favourite social medias via composer.io.
---
+++ source/popup.js
@@ -0,0 +1,64 @@
+var postbody = '';
+
+$(document).ready(function(){
+    $("#message").focus();
+    chrome.tabs.getSelected(null, function(tab){ 
+        if( tab.url.indexOf('chrome://') < 0 )
+        {
+		
+            var url = tab.url;
+			var title = tab.title;
+        
+            $("#message").val([title+" "+url]);
+        }
+    });
+    
+    $.get('http://composer.io/posts/add', function(data){
+        var inputtag = data.match(/<textarea[^>]*id="PostBody"><\/textarea>/);
+
+        $("#loading").hide();
+        if( inputtag != null ) {
+            postbody = data.match(/<textarea[^>]*id="PostBody"><\/textarea>/)[1];
+            $("#loggedin").fadeIn();
+            $("#message").select();
+        } else {
+            $("#loggedout").fadeIn();
+        
+        }
+    });
+
+    $("#composer").click(function(){     
+        $("#composer").attr("disabled","disabled");
+    
+        $.post('http://composer.io/posts/add',
+
+            {
+              'data[Post][body]': $('#message').val(),
+              'data[Post][services]': '',
+              postbody: postbody
+            }, function(){ window.close(); 
+            });
+    });
+    
+    function countMessage() {
+        var msg = $("#message").val();
+        if( msg.length == 1 )
+            $("#charcount").text(msg.length+" character");
+        else
+            $("#charcount").text(msg.length+" characters");
+            
+        if( msg.length > 140 )
+            $("#charcount").addClass("warning");
+        else
+            $("#charcount").removeClass("warning");
+    }
+    countMessage();
+    
+    var countTimer = null;
+    
+    $("#message").keydown(function(){
+        countTimer = setTimeout(countMessage,75);
+    }).keyup(function(){
+        clearTimeout(countTimer);
+    });
+});
